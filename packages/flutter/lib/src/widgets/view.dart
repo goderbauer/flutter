cc1410baee2431868474db379a3af43e3b7b0c6a
Michael Goderbauer <goderbauer@google.com>
2022-08-09 11:42:55 -0700

    pull pipelineowner out into _ViewState

--- packages/flutter/lib/src/widgets/view.dart
+++ packages/flutter/lib/src/widgets/view.dart
@@ -6,7 +6,7 @@ import 'package:flutter/rendering.dart';
 
 import 'framework.dart';
 
-class View extends StatelessWidget {
+class View extends StatefulWidget {
   const View({
     super.key,
     required this.view,
@@ -17,11 +17,28 @@ class View extends StatelessWidget {
   final Widget child;
 
   @override
+  State<View> createState() => _ViewState();
+}
+
+class _ViewState extends State<View> {
+  // Pulled out of _ViewElement so we can configure ViewHooks.
+  final PipelineOwner _pipelineOwner = PipelineOwner(
+    // TODO(goderbauer): This static access is annoying. Grab it from ViewHooks.pipelineOwner? How about updating it?
+    // Alternative: Adopt child could copy it from its parent if it is null.
+    onNeedVisualUpdate: RendererBinding.instance.ensureVisualUpdate,
+  );
+
+  @override
   Widget build(BuildContext context) {
+    final ViewHooksData hooks = ViewHooks.of(context);
     return _View(
-      view: view,
-      hooks: ViewHooks.of(context),
-      child: child,
+      view: widget.view,
+      hooks: hooks,
+      pipelineOwner: _pipelineOwner,
+      child: ViewHooks(
+        hooks: hooks.copyWith(pipelineOwner: _pipelineOwner),
+        child: widget.child,
+      ),
     );
   }
 }
@@ -31,11 +48,13 @@ class _View extends SingleChildRenderObjectWidget {
   const _View({
     required this.view,
     required this.hooks,
+    required this.pipelineOwner,
     required super.child,
   });
 
   final FlutterView view;
   final ViewHooksData hooks;
+  final PipelineOwner pipelineOwner;
 
   @override
   SingleChildRenderObjectElement createElement() => _ViewElement(this);
@@ -54,11 +73,9 @@ class _ViewElement extends SingleChildRenderObjectElement {
   @override
   RenderView get renderObject => super.renderObject as RenderView;
 
-  final PipelineOwner _pipelineOwner = PipelineOwner(
-    onNeedVisualUpdate: RendererBinding.instance.ensureVisualUpdate,
-  );
-
-  ViewHooksData get hooks => (widget as _View).hooks;
+  // TODO(goderbauer): inline these casts.
+  ViewHooksData get _hooks => (widget as _View).hooks;
+  PipelineOwner get _pipelineOwner => (widget as _View).pipelineOwner;
 
   @override
   void mount(Element? parent, Object? newSlot) {
@@ -73,7 +90,7 @@ class _ViewElement extends SingleChildRenderObjectElement {
       });
       return noRenderAncestor;
     }());
-    hooks.pipelineOwner.adoptChild(_pipelineOwner);
+    _hooks.pipelineOwner.adoptChild(_pipelineOwner);
     super.mount(parent, newSlot);
     renderObject.prepareInitialFrame();
     // TODO(goderbauer): semantics.
@@ -81,7 +98,7 @@ class _ViewElement extends SingleChildRenderObjectElement {
 
   @override
   void unmount() {
-    hooks.pipelineOwner.dropChild(_pipelineOwner);
+    _hooks.pipelineOwner.dropChild(_pipelineOwner);
     super.unmount();
   }
 
@@ -89,21 +106,22 @@ class _ViewElement extends SingleChildRenderObjectElement {
   void attachRenderObject(Object? newSlot) {
     assert(_pipelineOwner.rootNode == null);
     _pipelineOwner.rootNode = renderObject;
-    hooks.renderViewManager.addRenderView(renderObject);
+    _hooks.renderViewManager.addRenderView(renderObject);
   }
 
   @override
   void detachRenderObject() {
     assert(_pipelineOwner.rootNode == renderObject);
-    hooks.renderViewManager.removeRenderView(renderObject);
+    _hooks.renderViewManager.removeRenderView(renderObject);
     _pipelineOwner.rootNode = null;
   }
 
   @override
   void update(_View oldWidget) {
     super.update(oldWidget);
+    assert(oldWidget.pipelineOwner == _pipelineOwner);
     final ViewHooksData oldHooks = oldWidget.hooks;
-    final ViewHooksData newHooks = hooks;
+    final ViewHooksData newHooks = _hooks;
     if (oldHooks.pipelineOwner != newHooks.pipelineOwner) {
       oldHooks.pipelineOwner.dropChild(_pipelineOwner);
       newHooks.pipelineOwner.adoptChild(_pipelineOwner);
@@ -112,7 +130,7 @@ class _ViewElement extends SingleChildRenderObjectElement {
       oldHooks.renderViewManager.addRenderView(renderObject);
       newHooks.renderViewManager.removeRenderView(renderObject);
     }
-    // TODO(goderbauer): Do we need to mark anything dirty here? Or schedule a frame?
+    // TODO(goderbauer): Do we need to mark anything dirty here? Or schedule a frame? Probably no.
   }
 }
 
